0b86929ebdb96cc956e434f73778acca8d810829
Dav <dav.lazarte@gmail.com>
2020-09-25 22:58:33 -0300

    eliminamos next del back

--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,31 @@
-import express from 'express'
-import morgan from 'morgan'
-import cors from 'cors'
-import passport from 'passport'
-import passportMiddleware from './middlewares/passport'
-import authRoutes from './routes/auth.routes'
-import specialRoutes from './routes/special.routes'
-import farmaciaRoutes from './routes/farmacia.routes'
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import passport from "passport";
+import passportMiddleware from "./middlewares/passport";
+import authRoutes from "./routes/auth.routes";
+import specialRoutes from "./routes/special.routes";
+import farmaciaRoutes from "./routes/farmacia.routes";
 //initializations
-
 const app = express();
-
 //settings
-app.set('port', process.env.PORT || 4000);
+
+app.set("port", process.env.PORT || 4000);
 
 //middlewares
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(passport.initialize());
 passport.use(passportMiddleware);
 //routes
-app.get('/', (req, res) => {
-    res.send(`THE API is at http://localhost:${app.get('port')}`);
-})
+app.get("/", (req, res) => {
+  res.send(`THE API is at http://localhost:${app.get("port")}`);
+});
 
-app.use(authRoutes)
-app.use(specialRoutes)
-app.use(farmaciaRoutes)
+app.use(authRoutes);
+app.use(specialRoutes);
+app.use(farmaciaRoutes);
 
-export default app
+export default app;
